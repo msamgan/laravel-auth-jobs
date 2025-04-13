a042a7577d18b980e59c17c0f2bde3f6d7e470f3
Punyapal Shah <mrpunyapal@gmail.com>
2025-04-13 13:44:29 +0530

    wip

---
+++ src/Http/Middleware/AuthorizeJobs.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MrPunyapal\LaravelAuthJobs\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Context;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizeJobs
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            Context::add('laravel_auth_jobs_auth_id', Auth::id());
+            Context::add('laravel_auth_jobs_auth_guard', Auth::getDefaultDriver());
+        }
+
+        return $next($request);
+    }
+}
---
+++ src/Jobs/Middleware/AuthorizeJob.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MrPunyapal\LaravelAuthJobs\Jobs\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Context;
+
+class AuthorizeJob
+{
+    /**
+     * Process the queued job.
+     *
+     * @param  \Closure(object): void  $next
+     */
+    public function handle(object $job, Closure $next): void
+    {
+        $guard = Context::get('laravel_auth_jobs_auth_guard');
+        $id = Context::get('laravel_auth_jobs_auth_id');
+
+        Auth::guard($guard)->loginUsingId($id);
+
+        $next($job);
+    }
+}
